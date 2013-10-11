c472af36e1b0f7d7dc8aed8c2012fc62c4089dd9
Just <gwiesinger@gmail.com>
2013-10-10 20:46:59 -0700

    2010-10-10 20:46

--- web.rb
+++ web.rb
@@ -11,42 +11,27 @@ get '/' do
   haml :index
 end
 
-post '/charge_embedded' do
-  begin
+post '/charge' do
+#  begin
 
-    token = params[:stripeToken]
+    customer = Stripe::Customer.create(
+      :email => 'customer@example.com',
+      :card  => params[:stripeToken]
+    )
 
     charge = Stripe::Charge.create(
       :amount      => 1000, # in cents (isn't the amount passed into the POST body???)
       :description => 'Charge from XYZ Checkout',
       :currency    => 'usd',
-      :card        => token
+      :customer    => customer
     )
 
-    erb :charge
-
-  rescue Stripe::CardError => e
+#  rescue Stripe::CardError => e
   # Card has been declined
 
-  end
-end
+  haml :charge
 
-post '/charge' do
-  @amount = 500
-  
-  customer = Stripe::Customer.create(
-    :email => 'customer@example.com',
-    :card  => params[:stripeToken]
-  )
-
-  charge = Stripe::Charge.create(
-    :amount      => @amount,
-    :description => 'Charge from XYZ Checkout',
-    :currency    => 'eur',
-    :customer    => customer
-  )
-
-  erb :charge
+#  end
 end
 
 __END__
@@ -66,7 +51,7 @@ __END__
     %img{ :src => "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRaxhiip4BTdDkEh5JgZ-mOJ-hscEnYm7uE27Ml4BNAoCBH_bnH" }
   %p LOLcat, $20.00
   .button
-    %form{ :action => "", :method => "post" }
+    %form{ :action => "/charge", :method => "post" }
       %script{ :src => "https://checkout.stripe.com/v2/checkout.js", :class => "stripe-button", 
                :data => { :key => settings.publishable_key, :amount => "2000", :name => "Stars'n'Stripe */*", :description => "2 widgets ($20.00)", :image => "/unclesam.png" } }
 
@@ -76,13 +61,33 @@ __END__
     %img{ :src => "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRaxhiip4BTdDkEh5JgZ-mOJ-hscEnYm7uE27Ml4BNAoCBH_bnH" }
   %p LOLcat, $20.00
   .button
-    %form{ :action => "/charge_embedded", :method => "post" }
+    %form{ :action => "/charge", :method => "post" }
     %article
       %label{ :class => "amount" }
       %span Amount: $5.00
     %script{ :src => "https://checkout.stripe.com/v2/checkout.js",
              :class => "stripe-button", :data => { :key => settings.publishable_key } }
 
+%section.container
+  %script{ :src => "https://checkout.stripe.com/v2/" } Stripe.setPublishableKey(settings.publishable_key);
+  .content
+    %p Stripe Custom Form Checkout
+    %img{ :src => "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRaxhiip4BTdDkEh5JgZ-mOJ-hscEnYm7uE27Ml4BNAoCBH_bnH" }
+  %p LOLcat, donation-based
+  .form
+    %form{ :action => "", :method => "post", :id => "payment-form" }
+      %span{ :class => "payment-errors"}
+    %div{ :class => "form-row" }
+      %label
+        %span Card Number
+        %input{ :type => "text", :size => "20", :data => { :stripe => "number" } }
+    %div{ :class => "form-row" }
+      %label
+        %span Expiration (MM/YYYY)
+        %input{ :type => "text", :size => "2", :data => { :stripe => "exp-month" } }
+      %span /
+      %input{ :type => "text", :size => "4", :data => { :stripe => "exp-year" } }
+    %button{ :type => "submit"} Submit Payment
 
 @@charge
-  <h2>Thanks, you paid <strong>$5.00</strong>!</h2>
+%h2 Thanks, you paid <strong>$5.00</strong>!
